dda1de95ccc321ecd6d74e121feeafbf0f251d90
Zane Hitchcox <zwhitchcox@gmail.com>
2022-10-12 13:15:05 -0400

    fix touchpad

--- linux/wm/gnome.nix
+++ linux/wm/gnome.nix
@@ -9,7 +9,15 @@
       layout = "us";
       xkbVariant = "";
       # Enable touchpad support.
-      libinput.enable = true;
+      libinput = {
+        enable = true;
+        touchpad = {
+          tapping = true;
+          tappingDragLock = true;
+        };
+      };
+      synaptics.tapButtons = true;
+
       # Enable the Gnome desktop manager
       displayManager.gdm.enable    = true;
       desktopManager.gnome.enable = true;
