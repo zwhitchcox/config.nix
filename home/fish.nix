35e8fcc2c97f24c7f1ef3123489c08ba39011d3e
Zane Hitchcox <zwhitchcox@gmail.com>
2022-10-17 11:01:51 -0400

    remove theme warning

--- home/fish.nix
+++ home/fish.nix
@@ -137,21 +137,22 @@ in
     set -g fish_greeting ""
     ${pkgs.thefuck}/bin/thefuck --alias | source
 
-    # Run function to set colors that are dependent on `$term_background` and to register them so
-    # they are triggerd when the relevent event happens or variable changes.
-    set-shell-colors
+# comment out until setting theme
+#     # Run function to set colors that are dependent on `$term_background` and to register them so
+#     # they are triggerd when the relevent event happens or variable changes.
+#     set-shell-colors
 
-    # Set Fish colors that aren't dependent the `$term_background`.
-    set -g fish_color_quote        cyan      # color of commands
-    set -g fish_color_redirection  brmagenta # color of IO redirections
-    set -g fish_color_end          blue      # color of process separators like ';' and '&'
-    set -g fish_color_error        red       # color of potential errors
-    set -g fish_color_match        --reverse # color of highlighted matching parenthesis
-    set -g fish_color_search_match --background=yellow
-    set -g fish_color_selection    --reverse # color of selected text (vi mode)
-    set -g fish_color_operator     green     # color of parameter expansion operators like '*' and '~'
-    set -g fish_color_escape       red       # color of character escapes like '\n' and and '\x70'
-    set -g fish_color_cancel       red       # color of the '^C' indicator on a canceled command
+#     # Set Fish colors that aren't dependent the `$term_background`.
+#     set -g fish_color_quote        cyan      # color of commands
+#     set -g fish_color_redirection  brmagenta # color of IO redirections
+#     set -g fish_color_end          blue      # color of process separators like ';' and '&'
+#     set -g fish_color_error        red       # color of potential errors
+#     set -g fish_color_match        --reverse # color of highlighted matching parenthesis
+#     set -g fish_color_search_match --background=yellow
+#     set -g fish_color_selection    --reverse # color of selected text (vi mode)
+#     set -g fish_color_operator     green     # color of parameter expansion operators like '*' and '~'
+#     set -g fish_color_escape       red       # color of character escapes like '\n' and and '\x70'
+#     set -g fish_color_cancel       red       # color of the '^C' indicator on a canceled command
 
     function __fish_command_not_found_handler --on-event="fish_command_not_found"
       ${pkgs.bashInteractive}/bin/bash -c \
